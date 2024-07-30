c38a3ea4b5f1e543daad530a306046fc3eee1340
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2024-07-30 18:16:10 +0200

    add new contact / save function for firebase

--- js/firebase.js
+++ js/firebase.js
@@ -5,3 +5,17 @@ async function getData(path = "") {
     let response = await fetch(BASE_URL + path + ".json");
     return (responseToJson = await response.json());
 }
+
+/* save new contact in firebase */
+
+async function saveData(path = "", data) {
+    console.log(`Saving data to path: ${path}`);
+    await fetch(`${BASE_URL}${path}.json`, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    console.log('Data saved successfully');
+}
