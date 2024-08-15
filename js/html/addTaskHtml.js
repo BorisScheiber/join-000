e58b6b399e817d5f8f97a84752081b2a703540a1
Boris Scheiber <scheiberboris@hotmail.com>
2024-08-15 13:01:02 +0200

    added dots to img src

--- js/html/addTaskHtml.js
+++ js/html/addTaskHtml.js
@@ -9,9 +9,9 @@
     li.innerHTML = `
       <div ondblclick="editSubtask(this)" class="subtask-text"><li>${subtaskText}</li></div>
       <div class="edit-delete-icons" style="display: none;">
-        <img src="/assets/icons/edit.svg" alt="Edit" onclick="editSubtask(this)">
+        <img src="./assets/icons/edit.svg" alt="Edit" onclick="editSubtask(this)">
         <div class="vertical-line"></div>
-        <img src="/assets/icons/delete.svg" alt="Delete" onclick="deleteSubtask(this)">
+        <img src="./assets/icons/delete.svg" alt="Delete" onclick="deleteSubtask(this)">
       </div>
     `;
     return li;
@@ -29,9 +29,9 @@
         <div class="edit-div">
           <input type="text" class="input-field-editing" value="${subtaskText}">
           <div class="edit-delete">
-            <img src="/assets/icons/done.svg" alt="Done" onclick="saveSubtask(this)">
+            <img src="./assets/icons/done.svg" alt="Done" onclick="saveSubtask(this)">
             <div class="vertical-line"></div>
-            <img src="/assets/icons/delete.svg" alt="Delete" onclick="deleteSubtask(this)">
+            <img src="./assets/icons/delete.svg" alt="Delete" onclick="deleteSubtask(this)">
           </div>
         </div>
       </div>
@@ -48,9 +48,9 @@
     return `
       <div ondblclick="editSubtask(this)" class="subtask-text"><li>${newText}</li></div>
       <div class="edit-delete-icons" style="display: none;">
-        <img src="/assets/icons/edit.svg" alt="Edit" onclick="editSubtask(this)">
+        <img src="./assets/icons/edit.svg" alt="Edit" onclick="editSubtask(this)">
         <div class="vertical-line"></div>
-        <img src="/assets/icons/delete.svg" alt="Delete" onclick="deleteSubtask(this)">
+        <img src="./assets/icons/delete.svg" alt="Delete" onclick="deleteSubtask(this)">
       </div>
     `;
   }
